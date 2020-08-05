bd1c9dd803f66e2e2d6cd6047046209642f934ae
dhruv-dc <53862511+dhruv-dc@users.noreply.github.com>
2020-08-05 11:40:06 +0530

    Add functionality

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
   return (
     <div className="note">
-      <h1>This is title</h1>
-      <p>This is the content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
     </div>
   );
 }
